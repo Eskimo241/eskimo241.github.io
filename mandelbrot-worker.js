d3a4381e0ff6b27c700847bd2a039be5cb2328fa
Eskimo241 <eskimo241pro@gmail.com>
2024-01-09 11:38:12 +0100

    Add files via upload

--- mandelbrot-worker.js
+++ mandelbrot-worker.js
@@ -1,5 +1,5 @@
 self.onmessage = function(e) {
-    const { width, height, offsetX, offsetY, zoom } = e.data;
+    const { width, height, offsetX, offsetY, zoom, colorcolor } = e.data;
 
     function mandelbrot(c_re, c_im, max_iter) {
         let x = 0, y = 0, r2 = 0;
@@ -29,7 +29,8 @@ self.onmessage = function(e) {
                 mandelbrotData[(y + 1) * width + x] === 1000 ||
                 mandelbrotData[y * width + (x - 1)] === 1000 ||
                 mandelbrotData[y * width + (x + 1)] === 1000);
-            const color = m === 1000 ? [0, 0, 0, 255] : (isEdge ? [255, 0, 0, 255] : [0, 0, 0, 255]);
+            //const color = m === 1000 ? [0, 0, 0, 255] : (isEdge ? [255, 0, 0, 255] : [0, 0, 0, 255]);
+            const color = m === 1000 ? [0, 0, 0, 255] : (isEdge ? colorcolor : [0, 0, 0, 255]);
             imageData.data.set(color, (y * width + x) * 4);
         }
     }
